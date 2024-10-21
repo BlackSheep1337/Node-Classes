f0205995ef0d505053790516c5d21014e4ef670f
Ale Pereira <naitwa-alexandre@gmail.com>
2024-10-21 18:09:28 -0300

    express class

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`server listening on port:${PORT}`));
+
+
+app.get('/', (req, res) => {
+  //res.send('<p>Home page</p>'); 
+  res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+  //res.send('<p>About page</p>');
+  res.sendFile('./views/about.html', { root: __dirname });
+});
+
+app.get('/about-us', (req, res) => {
+  res.redirect = ('/about');
+});
+
+
+//MUST BE IN THE BOTTOM
+app.use((req, res) => {
+  res.status(404)
+    .sendFile('/views/404.html', { root: __dirname });
+});
