847f407830df08b347ac6c07d5e6551b7efbc962
Ale Pereira <naitwa-alexandre@gmail.com>
2024-12-05 12:04:37 -0300

    event from the book

--- node-express-course/app.js
+++ node-express-course/app.js
@@ -1,16 +1,36 @@
-const { readFile } = require('node:fs');
+// const { readFile } = require('node:fs');
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
-    });
-  });
-}
+// readFile('./etc/passwd/pass.txt', function(err, result) {
+//   console.log(result.toString());
+// } );
 
-getText('./content/first.txt')
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, 'utf-8', (err, data) => {
+//       if (err) {
+//         reject(err);
+//       }
+//       resolve(data);
+//     });
+//   });
+// }
+
+// getText('./content/first.txt')
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+// setTimeout(function() {
+//   console.log('setTimeout at '+new Date().toTimeString());
+// }, 1000);
+
+const http = require('node:http');
+
+const content = '&lt;html&gt;&lt;body&gt;&lt;p&gt;Hello World&lt;/p&gt;&lt;script type=”text/javascript”'
+  + '>alert(“Hi!”);&lt;/script&gt;&lt;/body&gt;&lt;/html&gt;'
+    
+http.createServer((request, response) => {
+  response.end(content);
+}).listen(8080, 'localhost');
+
+console.log('Server running at http://localhost:8080/.');
+
